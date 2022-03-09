4ab8a6f44104f6057db43a1231ecfb688ce93156
ZsofiErdodi <erdodizs@gmail.com>
2022-03-09 19:00:57 +0100

    Teszt panel

---
+++ src/main/java/com/example/liftproject/Vezerlo.java
@@ -0,0 +1,90 @@
+package com.example.liftproject;
+
+public class Vezerlo {
+
+    enum direction {UP, DOWN;};
+    private short floor;
+    private direction direction;
+    private boolean isMoving;
+    private boolean isOpenable;
+    private boolean isCloseable;
+    private boolean openDoorNow;
+
+    public boolean isOpenable() {
+        return isOpenable;
+    }
+
+    public void setOpenable(boolean openable) {
+        isOpenable = openable;
+    }
+
+    public boolean isCloseable() {
+        return isCloseable;
+    }
+
+    public void setCloseable(boolean closeable) {
+        isCloseable = closeable;
+    }
+
+    public boolean isOpenDoorNow() {
+        return openDoorNow;
+    }
+
+    public void setOpenDoorNow(boolean openDoorNow) {
+        this.openDoorNow = openDoorNow;
+    }
+
+
+    public short getFloor() {
+        return floor;
+    }
+
+    public void setFloor(short floor) {
+        this.floor = floor;
+    }
+
+    public Vezerlo.direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vezerlo.direction direction) {
+        this.direction = direction;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public void setMoving(boolean moving) {
+        isMoving = moving;
+    }
+
+    public Vezerlo()
+    {
+
+    }
+
+    public void GetLiftDoorState(boolean a, boolean b, boolean c)
+    {
+        isOpenable = a;
+        isCloseable = b;
+        openDoorNow = c;
+    }
+
+    public boolean isWeightLimitReached(boolean a)
+    {
+        return a;
+    }
+
+    public void GetLiftPositionInfo(direction d, boolean b, short f)
+    {
+        setDirection(d);
+        setMoving(b);
+        setFloor(f);
+    }
+
+    public boolean isUPSWorking(boolean a)
+    {
+        return a;
+    }
+}
